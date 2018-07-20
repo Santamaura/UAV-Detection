7c7ac5a2318b89f571b8aaf26deea9916611b8c7
Shashwat Pandey <s7pandey@edu.uwaterloo.ca>
2018-07-20 17:13:31 -0400

    Update map.js

--- routes/map.js
+++ routes/map.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('map');
+  res.render('map', {'title':'Map'});
 });
 
 module.exports = router;
