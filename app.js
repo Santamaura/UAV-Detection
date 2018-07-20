9673fb67157825b1ee17f5966548cd89cf266f7f
Shashwat Pandey <s7pandey@edu.uwaterloo.ca>
2018-07-20 14:31:25 -0400

    new changes

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var input = require('./routes/input');
+var map = require('./routes/map');
 
 
 var app = express();
@@ -26,7 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/input',input);
-
+app.use('/map',map);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
