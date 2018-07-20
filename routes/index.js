a0362fc9d57558326146867a8517c2bae05de350
Shashwat Pandey <s7pandey@edu.uwaterloo.ca>
2018-07-20 00:19:11 -0400

    name change

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Jarvis' });
+  res.render('index', { title: 'FYDP' });
 });
 
 module.exports = router;
