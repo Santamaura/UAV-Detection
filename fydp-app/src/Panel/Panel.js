8fedc50740b2e38bf8314b666f02bb4f3fbaaa07
Alex Santamaura <alexsantamaura@gmail.com>
2019-03-05 21:54:52 -0500

    dded panel file

---
+++ fydp-app/src/Panel/Panel.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import moment from 'moment';
+
+export default class Panel extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const {systemStats} = this.props.items;
+        return (
+            <div>
+                <h2>{'Current Scanning Frequency: ' + systemStats.currentScanFreq}</h2>
+                <h2>{'Current Time: ' + moment.unix(currentTime).format("MM/DD/YYYY")}</h2>
+                <h2>{'Current Tracking Frequency: ' + systemStats.currentTrackFreq}</h2>
+                <h2>{'Flow Graph Version: ' + systemStats.flowgraphVersion}</h2>
+                <h2>{'Software Version: ' + systemStats.softwareVersion}</h2>
+                <h2>{'System Latitude: ' + systemStats.systemLat}</h2>
+                <h2>{'System Longitude: ' + systemStats.systemLon}</h2>
+                <h2>{'Uptime: ' + systemStats.uptime}</h2>
+            </div>
+        )
+    }
+}
