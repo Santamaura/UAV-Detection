042f77687c4ae9534c75dd7a7d5bcc5da5f6bf8a
Alex <alexsantamaura@gmail.com>
2019-03-13 17:46:33 -0400

    radius change

--- fydp-app/src/Map/Circle/Circle.js
+++ fydp-app/src/Map/Circle/Circle.js
@@ -6,7 +6,7 @@ export class Circle extends Component {
     }
     render() {
         const { radius } = this.props;
-        let scale = (radius * 40).toString();
+        let scale = (radius * 10).toString();
         return(
             <svg height="100" width="100">
                 <circle cx="50" cy="50" r={scale} stroke="black" stroke-width="2" fill="lightgrey" fill-opacity="0.2" />
