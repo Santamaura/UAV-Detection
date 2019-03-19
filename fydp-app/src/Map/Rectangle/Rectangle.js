30882112f373f1e0200c891d60358b15961d3d53
Shashwat Pandey <s7pandey@edu.uwaterloo.ca>
2019-03-18 20:24:12 -0400

    I FIXED IT BABY

--- fydp-app/src/Map/Rectangle/Rectangle.js
+++ fydp-app/src/Map/Rectangle/Rectangle.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {BaseControl} from 'react-map-gl';
 import * as d3 from 'd3';
 
+var rect = null;
 export class Rectangle extends BaseControl {
     constructor(props) {
         super(props);
@@ -16,11 +17,18 @@ export class Rectangle extends BaseControl {
     getLatLng(x0,y0, x1, y1){
         return [this._context.viewport.unproject([x0, y0]),this._context.viewport.unproject([x1, y1])];
     }
-    drawRect() {
+    changeColour(style){
+        console.log(style);
+        console.log("///////////")
+        if(rect){
+            rect.attr("fill", style.fill);
+        }
+    }
+    drawRect(style=null) {
         const {callback} = this.props;
         const {convertToLatLng} = this.state;
-        var rect = null;
-        var svg = d3.select("#rectangle").append("svg")
+        // var rect = null;
+        var svg = d3.select("#rectangle").select("svg")
             .attr("width", 10000)
             .attr("height", 10000)
             .on("mousedown", function(){
@@ -32,7 +40,8 @@ export class Rectangle extends BaseControl {
                     .attr("x", m[0])
                     .attr("y", m[1])
                     .attr("height", 0)
-                    .attr("width", 0);
+                    .attr("width", 0)
+                    .attr("opacity", 0.3);
     
                 svg.on("mousemove", function(){
                     var m = d3.mouse(this);
@@ -46,11 +55,17 @@ export class Rectangle extends BaseControl {
                 svg.on("mousemove", null);
                 var points = convertToLatLng(parseInt(rect.attr("x")), parseInt(rect.attr("y")), parseInt(rect.attr("x")) + parseInt(rect.attr("width")), parseInt(rect.attr("y")) + parseInt(rect.attr("height")));
                 callback(points);
+                if(style && rect){
+                    rect.attr("fill", style.fill)
+                }
             });
         
     }
     _render() {
+        const {style} = this.props;
+        {this.changeColour(style)}
         return <div id={"rectangle"}>
+        <svg></svg>
          </div>;
     }
 }
