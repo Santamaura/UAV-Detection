0eff68f3ffc0f8eeaf24ee9ee629d63843da6e43
Shashwat Pandey <s7pandey@edu.uwaterloo.ca>
2019-03-20 01:14:21 -0400

    Appjs back to local

--- fydp-app/src/App.js
+++ fydp-app/src/App.js
@@ -1,17 +1,6 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
-import items_1 from './video_data_1.json';
-import items_2 from './video_data_2.json';
-import items_3 from './video_data_3.json';
-import items_4 from './video_data_4.json';
-import items_5 from './video_data_5.json';
-import items_6 from './video_data_6.json';
-import items_7 from './video_data_7.json';
-import items_8 from './video_data_8.json';
-import items_9 from './video_data_9.json';
-import items_10 from './video_data_10.json';
-import items_11 from './video_data_11.json';
-
+import items from './sample.json';
 import './App.css';
 import Map from './Map/Map.js';
 import Panel from './Panel/Panel.js';
@@ -20,7 +9,6 @@ import Drawer from '@material-ui/core/Drawer';
 import TextField from '@material-ui/core/TextField';
 
 const drawerWidth = 240;
-var index = 0;
 const drawerStyles = theme => ({
   drawerPaper: {
     position: 'relative',
@@ -45,10 +33,7 @@ class App extends Component {
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 componentDidMount() {
-  this.timer = setInterval(()=>{
-    index = index % 11 + 1; 
-    this.getItems(index);
-  }, 3000); 
+  this.timer = setInterval(() => this.getItems(), 500);
 }
 
 componentWillUnmount() {
@@ -63,45 +48,8 @@ handleSubmit(event) {
   alert('A radius was submitted: ' + this.state.radiusVal);
   event.preventDefault();
 }
-getItems(index) {
-  index = index % 11 + 1;
-  console.log(index);
-  switch(index){
-    case 1:
-    this.setState({items: items_1});
-    break;
-    case 2:
-    this.setState({items: items_2});
-    break;
-    case 3:
-    this.setState({items: items_3});
-    break;
-    case 4:
-    this.setState({items: items_4});
-    break;
-    case 5:
-    this.setState({items: items_5});
-    break;
-    case 6:
-    this.setState({items: items_6});
-    break;
-    case 7:
-    this.setState({items: items_7});
-    break;
-    case 8:
-    this.setState({items: items_8});
-    break;
-    case 9:
-    this.setState({items: items_9});
-    break;
-    case 10:
-    this.setState({items: items_10});
-    break;
-    case 11:
-    this.setState({items: items_11});
-    break;
-  }
-  
+getItems() {
+  this.setState({items: items});
     // fetch("http://192.168.3.4:5000/")
     //     .then(result => result.json())
     //     .then(result => this.setState({ items: result }))
